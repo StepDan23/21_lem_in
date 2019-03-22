189ba0a5ab67a4d1cef5acabdc913f996fa79239
How_r_u <kirtrishin@gmail.com>
2019-03-22 12:13:13 +0300

    add optimizations in 1 tube and add prototype of many tubes

--- test.c
+++ test.c
@@ -10,7 +10,6 @@ int main(void)
 	int len = 6;
 	int num_of_ants = 3;
 	int	num_of_ant_in_tube = 1;
-	int	ant_on_start = 0;
 	int	count_temp = num_of_ants;
 	int count_end = 0;
 	int i = -1;
@@ -21,13 +20,11 @@ int main(void)
 		zone[i] = 0;
 	while (count_end != num_of_ants - 1)
 	{
-		(j < len) ? j++ : 0;
-		i = j;
-
+		i = (j < len) ? ++j : j;
 		(j == len) ? count_end++ : 0;
 		while (i > 0)
 		{
-			(j ) ? (zone[i] = zone[i - 1]) : 0;
+			(j) ? (zone[i] = zone[i - 1]) : 0;
 			(i < len && zone[i]) ? printf("L%d-%d ", zone[i], arr[i]) : 0;
 			i--;
 		}
@@ -35,13 +32,11 @@ int main(void)
 			zone[0] = 0;
 		if (count_temp > 0)
 		{
-			(j > 0) ? (zone[1] = zone[0]) : 0;
 			zone[0] = num_of_ant_in_tube;
-			printf("L%d-%d ", zone[0], arr[0]);
 			num_of_ant_in_tube++;
 			count_temp--;
 		}
-		putchar('\n');
+		(j > 0) ? putchar('\n') : 0;
 	}
 	return (0);
 }
