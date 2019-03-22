586c912156fb3afe15a755a63d98305ed0ebe4e8
Kirill <kirtrishin@gmail.com>
2019-03-22 14:01:30 +0300

    it seems that two ways works, at least whan length the same

--- test2.c
+++ test2.c
@@ -21,14 +21,14 @@ int	ft_is_finished_all(int *start, int *end, int len)
 int main(void)
 {
 	int arr[2][6] = {
-		{0, 1, 2, 3, 4, 5},
-		{0, 6, 7, 8, 9, 5}
+		{0, 11, 12, 13, 14, 5},
+		{0, 26, 27, 28, 29, 5}
 	};
 	int	ways_count = 2;
-	int	len[] = {6, 7};
-	int	ant_start[] = {9, 8};
+	int	len[] = {6, 6};
+	int	ant_start[] = {9, 9};
 	int	ant_finished[] = {0, 0};
-	int	ant_left[] = {9, 8};
+	int	ant_left[] = {9, 9};
 	int	num_of_ant_in_tube = 1;
 	int	k;
 	int	i;
@@ -54,7 +54,6 @@ int main(void)
 			{
 				(j[k]) ? (zone[k][i] = zone[k][i - 1]) : 0;
 				(i < len[k] && zone[k][i]) ? printf("L%d-%d ", zone[k][i], arr[k][i]) : 0;
-
 				i--;
 			}
 			if (ant_left[k] == 0)
@@ -67,13 +66,6 @@ int main(void)
 			}
 		}
 		k = (k + 1) % ways_count;
-		//if (k == 0)
-		//{
-		//	i = j[k];
-		//	while (i >= 0)
-		//		printf("L%d-%d ", zone[k][i], arr[k][i++]);
-		//	printf("         ");
-		//}
 		(k == 0) ? putchar('\n') : 0;
 	}
 	return (0);
